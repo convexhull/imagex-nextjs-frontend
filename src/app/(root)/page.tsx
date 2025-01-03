49a87ed1d054c03fdec0076e213004f2a5c5af08
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-01-04 04:47:38 +0530

    Add credentials provider for auth, install zod for validation

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,10 @@
-export default function Home() {
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
+      <h2>{session && "Logged in!"}</h2>
       <h1>Home</h1>
     </div>
   );
--- src/auth.ts
+++ src/auth.ts
@@ -5,27 +5,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       credentials: {
-        email: { label: "Email" },
-        password: { label: "Password", type: "password" },
+        email: {},
+        password: {},
       },
       async authorize({ email, password }) {
-        console.log(email, password);
         const response = await fetch("http://localhost:4000/users/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({ email, password }),
-          cache: "no-store",
         });
-        console.log(await response.json());
-        // if (!response.ok) return null;
-
-        return (await response.json()) ?? null;
+        const user = await response.json();
+        if (user.token) {
+          return user;
+        }
+        return null;
       },
     }),
   ],
-  // pages: {
-  //   signIn: "/login",
-  // },
+  pages: {
+    signIn: "/login",
+  },
 });
