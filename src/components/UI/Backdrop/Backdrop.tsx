1b807bc5c42171c6eafdb19e73516fd1d3269c0d
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-01-07 19:53:35 +0530

    Add <Backdrop /> for dropdown menu

---
+++ src/components/UI/Backdrop/Backdrop.tsx
@@ -0,0 +1,23 @@
+"use client";
+// Styles
+import classes from "./Backdrop.module.css";
+
+type BackdropProps = {
+  opacity: string;
+  hideBackdrop: () => void;
+};
+
+const Backdrop = ({ opacity, hideBackdrop }: BackdropProps) => {
+  const backdropClasses = [classes["Backdrop"]];
+  if (opacity === "translucent") {
+    backdropClasses.push(classes["backdrop--translucent"]);
+  }
+  if (opacity === "transparent") {
+    backdropClasses.push(classes["backdrop--transparent"]);
+  }
+  return (
+    <div className={backdropClasses.join(" ")} onClick={hideBackdrop}></div>
+  );
+};
+
+export default Backdrop;
