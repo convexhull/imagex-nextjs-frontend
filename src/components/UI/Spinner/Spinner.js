17e927a87675eaf30e6d695e9f42fe0b8124e076
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-02-27 13:13:25 +0530

    Create <Spinner /> for loading

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from "./Spinner.module.css";
+
+const Spinner = () => {
+  return (
+    <div className={classes.container}>
+      <div class={classes["lds-spinner"]}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
