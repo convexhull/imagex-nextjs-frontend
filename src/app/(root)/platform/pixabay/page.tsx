b18e8ef0913893a09beaad37ee795d430fdce8a0
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-02-28 16:37:18 +0530

    Create homepage for unsplash and pixabay platform

--- src/app/(root)/platform/pixabay/page.tsx
+++ src/app/(root)/platform/pixabay/page.tsx
@@ -1,5 +1,22 @@
-const Pixabay = () => {
-  return <div>Pixabay</div>;
+// Components
+import HeroSection from "@/components/HeroSection/HeroSection";
+// Styles
+import classes from "./page.module.css";
+// Types
+import { Platform } from "@/lib/types";
+
+const PixabayHome = () => {
+  return (
+    <div className={classes["hero-container"]}>
+      <HeroSection
+        platform={Platform.PIXABAY}
+        title="Pixabay"
+        subtitle1="Stunning free images & royalty free stock"
+        subtitle2="Over 1.8 million+ high quality stock images and videos shared by our talented community."
+        subtitle3="Trending: nature, wallpaper, food, business, love, computer, money, beach, office, car, sky, summer"
+      />
+    </div>
+  );
 };
 
-export default Pixabay;
+export default PixabayHome;
--- src/app/(root)/platform/unsplash/page.tsx
+++ src/app/(root)/platform/unsplash/page.tsx
@@ -1,5 +1,22 @@
-const Unsplash = () => {
-  return <div>Unsplash</div>;
+// Components
+import HeroSection from "@/components/HeroSection/HeroSection";
+// Types
+import { Platform } from "@/lib/types";
+// Styles
+import classes from "./page.module.css";
+
+const UnsplashHome = () => {
+  return (
+    <div className={classes["hero-container"]}>
+      <HeroSection
+        platform={Platform.UNSPLASH}
+        title="Unsplash"
+        subtitle1="Your source of freely-usable images."
+        subtitle2="Powered by creators everywhere."
+        subtitle3="Powered by creators everywhere."
+      />
+    </div>
+  );
 };
 
-export default Unsplash;
+export default UnsplashHome;
